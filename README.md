eabe88382205fec4a0998cb03cecb78213110df3
nulLeeKH <i_am@nulleekh.com>
2019-09-29 14:27:53 +0900

    [UPDATE] Release v1.0.0-alpha

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ Python script for download onlineschool textbooks for Korean students easily.
 - This script is for Korea National Education Agency's onlineschool Program.
 	- [Website for High School](http://hs.onlineschool.or.kr/)
 	- [Website for Middle School](http://ms.onlineschool.or.kr/)
+
+## Patch Note
+
+### v1.0.0-alpha
+- Initial release
---
+++ src/onlineschool-textbook-downloader/onlns_txtbk_downloader.py
@@ -0,0 +1,36 @@
+from requests import get  
+
+
+def download(downloading_file_url, downloaded_file_name = None):
+    with open(downloaded_file_name, "wb") as file:
+        response = get(downloading_file_url)
+        file.write(response.content)
+
+
+if __name__ == '__main__':
+    # Example for following link [http://bf2009down.kedicdn.re.kr/2015hs/for2/spa1/webbook_01.zip]
+
+    curriculum = "2015"
+    # Enter the year of your curriculum
+    school_type = "hs"
+    # Enter type of your school
+    subject_type = "for2"
+    # Enter type of your subject
+    subject_name = "spa1"
+    # Enter name of your subject
+    file_name = "webbook"
+    # Enter name of your textbook
+    file_type = "zip"
+    # Enter type of your textbook
+
+    endpoint = 17
+    # Enter number of your lessons
+
+    for i in range (1, endpoint+1):
+        if i < 10:
+            url = "http://bf2009down.kedicdn.re.kr/" + curriculum + school_type + "/" + subject_type + "/" + subject_name + "/" + file_name + "_0" + str(i) + "." + file_type
+            download(url, file_name + "_0" + str(i) + "." + file_type)
+
+        else:
+            url = "http://bf2009down.kedicdn.re.kr/" + curriculum + school_type + "/" +subject_type + "/" + subject_name + "/" + file_name + "_" + str(i) + "." + file_type
+            download(url, file_name + "_" + str(i) + "." + file_type)
